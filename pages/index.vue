ad41b5f3f5df331b79cdb939cf8def9dc655e9d1
Yang <y.liu@esciencecenter.nl>
2021-10-21 16:57:14 +0200

    basic navigation from index page to each region page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,28 @@
 <template>
-  <Tutorial/>
+<div>        
+  <div v-for="(region,key) in regions" :key="key" class="flex flex-wrap">
+    <div class="w-screen">
+      <NuxtLink :to="key">
+        <h3 class="text-2xl p-2 m-2">
+          {{ region.name }}
+        </h3>
+      </NuxtLink>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      regions: []
+    }
+  },
+  async mounted () {
+    const index = await this.$content('index').fetch()
+    this.regions = index.regions
+  }
+}
 </script>
